f616310b993199ff974b34c09bcb20a0dc6a758f
IT21834424 <it21834424@my.sliit.lk>
2024-03-21 19:51:58 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         required :true,
         unique:true,
     },
-    passward:{
+    password:{
         type :String,
         required :true,
         
